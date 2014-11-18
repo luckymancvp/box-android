274289c1e60b9e8900646f28b39a73e0c3bbdb2f
luckymancvp <luckymancvp@gmail.com>
2014-11-18 08:19:20 +0700

    some changes

--- update-sdk.sh
+++ update-sdk.sh
@@ -3,4 +3,6 @@
 export ANDROID_UPDATE_FILTER="`tr '\n' ',' <$WERCKER_SOURCE_DIR/filter.list`"
 echo "export ANDROID_UPDATE_FILTER=\"${ANDROID_UPDATE_FILTER}\"" | sudo tee -a $profile
 
+# echo list sdk
+android list sdk
 expect -f /pipeline/build/android-update.exp "$ANDROID_UPDATE_FILTER"
